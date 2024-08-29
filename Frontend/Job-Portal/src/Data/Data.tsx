8b4a2e07e3e1aeab02a5fcdf1a92cdd349a89630
ShankarSharma1601 <shankarsharma1601@gmail.com>
2024-08-30 00:17:59 +0530

    Design Landing Page

---
+++ Frontend/Job-Portal/src/Data/Data.tsx
@@ -0,0 +1,115 @@
+// export const companies = ["Google" , "Amazon" , "Figma" , "Netflix" , "Meta" , "Microsoft" , "Pinterest" , "Slack" , "Oracle" , "Walmart"];
+
+export const companies = [{
+    name : "Google",
+    src : "https://w7.pngwing.com/pngs/63/1016/png-transparent-google-logo-google-logo-g-suite-chrome-text-logo-chrome.png",
+    alt : "Google"
+},
+{
+    name : "Amazon",
+    src : "https://w7.pngwing.com/pngs/489/527/png-transparent-amazon-logo-amazon-com-retail-customer-service-walmart-amazon-logo-thumbnail.png",
+    alt : "Amazon",
+},
+{
+    name : "Figma",
+    src : "https://w7.pngwing.com/pngs/911/515/png-transparent-figma-logo-brand-logos-brands-in-colors-icon-thumbnail.png",
+    alt : "Figma",
+},
+{
+    name : "Netflix",
+    src : "https://w7.pngwing.com/pngs/153/31/png-transparent-netflix-macos-bigsur-icon-thumbnail.png",
+    alt : "Netflix",
+},
+{
+    name : "Meta",
+    src : "https://w7.pngwing.com/pngs/36/959/png-transparent-meta-logo-facebook-social-media-chat-message-communication-icon-thumbnail.png",
+    alt : "Meta",
+},
+{
+    name : "Microsoft",
+    src : "https://w7.pngwing.com/pngs/827/526/png-transparent-microsoft-windows-microsoft-outlook-microsoft-office-microsoft-logo-s-text-rectangle-triangle-thumbnail.png",
+    alt : "Microsoft",
+},
+{
+    name : "Pinterest",
+    src : "https://w7.pngwing.com/pngs/56/300/png-transparent-logo-pinterest-computer-icons-symbol-miscellaneous-text-trademark-thumbnail.png",
+    alt : "Pinterest",
+},
+{
+    name : "Slack",
+    src : "https://w7.pngwing.com/pngs/345/302/png-transparent-chat-slack-slack-logo-social-media-icon-thumbnail.png",
+    alt : "Slack",
+},
+{
+    name : "Oracle",
+    src : "https://w7.pngwing.com/pngs/68/900/png-transparent-oracle-cloud-oracle-corporation-oracle-database-logo-netsuite-business-text-people-logo-thumbnail.png",
+    alt : "Oracle",
+},
+{
+    name : "Walmart",
+    src : "https://w7.pngwing.com/pngs/45/625/png-transparent-yellow-logo-illustration-walmart-logo-grocery-store-retail-asda-stores-limited-icon-walmart-logo-miscellaneous-company-orange-thumbnail.png",
+    alt : "Walmart",
+},
+{
+    name : "Atlassian",
+    src : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQsq9A-uezCsXnGQ18-cs8Ad7KRufEwr8BTIg&s",
+    alt : "Atlassian"
+}
+];
+
+export const jobCategory = [
+    {
+        src : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjvCE1V8oD75Pdee-EKe5l_ESUGvInLA414w&s",
+        name : "Sales",
+        desc : "small products and services to customers",
+        jobs : "900"
+    },
+    {
+        src : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGV34vbiV9aof-1hWjCnPsO9SiKGsVF750ng&s",
+        name : "Finance",
+        desc : "Manage financial records and transactions",
+        jobs : "700"
+    },
+    {
+        src : "https://e7.pngegg.com/pngimages/635/97/png-clipart-computer-icons-the-broadleaf-group-people-icon-miscellaneous-monochrome.png",
+        name : "Human Resource",
+        desc : "Recruit, manage and supplies company empoyees",
+        jobs : "600"
+    },
+    {
+        src : "https://as1.ftcdn.net/v2/jpg/02/33/72/96/1000_F_233729614_Vy89a8MJ4eDwG6ZXX7kRGbIgjOcBbSTx.jpg",
+        name : "Customer Support",
+        desc : "Assist customers with inquiries and issues",
+        jobs : "1.2K"
+    },
+    {
+        src : "https://e7.pngegg.com/pngimages/677/439/png-clipart-silhouette-illustration-of-man-web-development-php-software-developer-programmer-web-design-computer-user-icon-svg-miscellaneous-computer-programming.png",
+        name : "Software Engineers",
+        desc : "Develop any software you want",
+        jobs : "2.7K"
+    },
+    {
+        src : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGV34vbiV9aof-1hWjCnPsO9SiKGsVF750ng&s",
+        name : "Finance",
+        desc : "Manage financial records and transactions",
+        jobs : "700"
+    },
+    {
+        src : "https://e7.pngegg.com/pngimages/635/97/png-clipart-computer-icons-the-broadleaf-group-people-icon-miscellaneous-monochrome.png",
+        name : "Human Resource",
+        desc : "Recruit, manage and supplies company empoyees",
+        jobs : "600"
+    },
+    {
+        src : "https://as1.ftcdn.net/v2/jpg/02/33/72/96/1000_F_233729614_Vy89a8MJ4eDwG6ZXX7kRGbIgjOcBbSTx.jpg",
+        name : "Customer Support",
+        desc : "Assist customers with inquiries and issues",
+        jobs : "1.2K"
+    },
+    {
+        src : "https://e7.pngegg.com/pngimages/677/439/png-clipart-silhouette-illustration-of-man-web-development-php-software-developer-programmer-web-design-computer-user-icon-svg-miscellaneous-computer-programming.png",
+        name : "Software Engineers",
+        desc : "Develop any software you want",
+        jobs : "2.7K"
+    },
+];
